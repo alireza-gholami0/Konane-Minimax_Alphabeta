de09bee7591ed81b79451cf1c50c8a7b86daac48
ReyhaneBstp <reyhane.bstp@gmail.com>
2023-04-14 17:06:07 +0430

    change checkCorners func

--- Konane-Minimax_Alphabeta-main/KonaneGame2.py
+++ Konane-Minimax_Alphabeta-main/KonaneGame2.py
@@ -126,11 +126,12 @@ class KonaneGame2:
         for i in range (0,board.size-1):
             for j in range (0,board.size-1):
                 if board.game_board[i][j].piece == color :
-                    if i!=0 :
-                        if board.game_board[i-1][j].piece == self.opponent(color) and board.game_board[i+1][j].piece == self.opponent(color):
+
+                    if i!=0 and board.game_board[i-1][j].piece == self.opponent(color) :
+                        if i!=board.size-1 and board.game_board[i+1][j].piece == self.opponent(color) :
                             value += 20
-                    if j!=0 :
-                        if board.game_board[i][j-1].piece == self.opponent(color) and board.game_board[i][j+1].piece == self.opponent(color):
+                    if j!=0 and board.game_board[i][j-1].piece == self.opponent(color):
+                        if j!=board.size-1 and board.game_board[i][j+1].piece == self.opponent(color):
                             value += 20
 
         valid_moves_color = self.generate_all_possible_moves(board, color)
@@ -151,28 +152,28 @@ class KonaneGame2:
             allowed_move += self.check(0,0,0,2,1,self.opponent(color))
             allowed_move += self.check(0,0,2,0,1,self.opponent(color))
         if board.game_board[-1][-1].piece == color :
-            allowed_move += self.check(0,0,-1,-3,1,self.opponent(color))
-            allowed_move += self.check(0,0,-3,-1,1,self.opponent(color))
+            allowed_move += self.check(-1,-1,-1,-3,1,self.opponent(color))
+            allowed_move += self.check(-1,-1,-3,-1,1,self.opponent(color))
         if board.game_board[0][-1].piece == color :
-            allowed_move += self.check(0,0,0,-3,1,self.opponent(color))
-            allowed_move += self.check(0,0,2,-1,1,self.opponent(color))
+            allowed_move += self.check(0,-1,0,-3,1,self.opponent(color))
+            allowed_move += self.check(0,-1,2,-1,1,self.opponent(color))
         if board.game_board[-1][0].piece == color :
-            allowed_move += self.check(0,0,-1,2,1,self.opponent(color))
-            allowed_move += self.check(0,0,-3,0,1,self.opponent(color))
+            allowed_move += self.check(-1,0,-1,2,1,self.opponent(color))
+            allowed_move += self.check(-1,0,-3,0,1,self.opponent(color))
 
 
         if board.game_board[0][0].piece == self.opponent(color) :
             opponent_move += self.check(0,0,0,2,1,color)
             opponent_move += self.check(0,0,2,0,1,color)
         if board.game_board[-1][-1].piece== self.opponent(color) :
-            opponent_move += self.check(0,0,-1,-3,1,color)
-            opponent_move += self.check(0,0,-3,-1,1,color)
+            opponent_move += self.check(-1,-1,-1,-3,1,color)
+            opponent_move += self.check(-1,-1,-3,-1,1,color)
         if board.game_board[0][-1].piece == self.opponent(color) :
-            opponent_move += self.check(0,0,0,-3,1,color)
-            opponent_move += self.check(0,0,2,-1,1,color)
+            opponent_move += self.check(0,-1,0,-3,1,color)
+            opponent_move += self.check(0,-1,2,-1,1,color)
         if board.game_board[-1][0].piece == self.opponent(color) :
-            opponent_move += self.check(0,0,-1,2,1,color)
-            opponent_move += self.check(0,0,-3,0,1,color)
+            opponent_move += self.check(-1,0,-1,2,1,color)
+            opponent_move += self.check(-1,0,-3,0,1,color)
         
         point = len(allowed_move) - len(opponent_move)
         return point
